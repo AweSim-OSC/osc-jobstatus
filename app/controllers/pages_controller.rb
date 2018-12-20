700158a7307718c11704431a9f0ce6864137b4a9
Tyler Harris <tharris@osc.edu>
2018-12-20 16:22:54 -0500

    Display more useful error message when jobcluster query is not a valid cluster

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -92,44 +92,49 @@ class PagesController < ApplicationController
     errors = Array.new
     jobfilter = get_filter
     jobcluster = get_cluster
+    
+    if jobcluster.nil?
+      errors << "'#{params[:jobcluster]}' is not a defined cluster\n"
+      return { data: jobs, errors: errors }
+    else
+      OODClusters.each do |cluster|
 
-    OODClusters.each do |cluster|
+        if jobcluster == 'all' || cluster == OODClusters[jobcluster]
 
-      if jobcluster == 'all' || cluster == OODClusters[jobcluster]
+          b = cluster.job_adapter
 
-        b = cluster.job_adapter
-
-        begin
-          if jobfilter == 'user'
-            result = b.info_where_owner(OodSupport::User.new.name)
-          else
-            filter = Filter.list.find { |f| f.filter_id == jobfilter }
-            result = filter ? filter.apply(b.info_all) : b.info_all
-          end
-
-          # Only add the running jobs to the list and assign the host to the object.
-          #
-          # There is also curently a bug in the system where jobs with an empty array
-          # (ex. 6407991[].oak-batch.osc.edu) are not stattable, so we do a not-match
-          # for those jobs and don't display them.
-          result.each do |j|
-            if j.status.state != :completed && j.id !~ /\[\]/
-              jobs.push(Jobstatusdata.new(j, cluster))
+          begin
+            if jobfilter == 'user'
+              result = b.info_where_owner(OodSupport::User.new.name)
+            else
+              filter = Filter.list.find { |f| f.filter_id == jobfilter }
+              result = filter ? filter.apply(b.info_all) : b.info_all
             end
+
+            # Only add the running jobs to the list and assign the host to the object.
+            #
+            # There is also curently a bug in the system where jobs with an empty array
+            # (ex. 6407991[].oak-batch.osc.edu) are not stattable, so we do a not-match
+            # for those jobs and don't display them.
+            result.each do |j|
+              if j.status.state != :completed && j.id !~ /\[\]/
+                jobs.push(Jobstatusdata.new(j, cluster))
+              end
+            end
+          rescue => e
+            msg = "#{cluster.metadata.title || cluster.id.to_s.titleize}: #{e.message}"
+            logger.error "#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}"
+            errors << msg
           end
-        rescue => e
-          msg = "#{cluster.metadata.title || cluster.id.to_s.titleize}: #{e.message}"
-          logger.error "#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}"
-          errors << msg
         end
       end
-    end
 
-    # Sort jobs by username
-    jobs.sort_by! do |user|
-      user.username == OodSupport::User.new.name ? 0 : 1
+      # Sort jobs by username
+      jobs.sort_by! do |user|
+        user.username == OodSupport::User.new.name ? 0 : 1
+      end
     end
-
+    
     { data: jobs, errors: errors }
   end
 end
